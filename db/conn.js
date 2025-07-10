6cf21ed177fa7654a6c7ff080e323afc6bc50f09
ARTHUR ALEXANDRE ABBRUZZINI <arthur_abbruzzini@estudante.sesisenai.org.br>
2025-07-10 09:16:16 -0300

    segundo

--- db/conn.js
+++ db/conn.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize('trabalho_arthur','root','senai',{
-    host: 'localhost',
+const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS,{
+    host: process.env.DB_HOST,
     dialect: 'mysql',
-    port: 3306
+    port: process.env.PORT2
 })
 
 sequelize.authenticate()
